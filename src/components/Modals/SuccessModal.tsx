054127b4a9aec9e5814d20989e81b67bdff77fa7
lmangall <l.mangallon@gmail.com>
2024-06-14 13:04:39 +0200

    feat(modal)

---
+++ src/components/Modals/SuccessModal.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {Modal, Button, Placeholder} from "@telegram-apps/telegram-ui";
+
+type SuccessModalProps = {
+  onClose: () => void;
+};
+
+const SuccessModal: React.FC<SuccessModalProps> = ({onClose}) => {
+  return (
+    <Modal
+      header={<Modal.Header>Success</Modal.Header>}
+      trigger={null}
+      isOpen={true}
+    >
+      <div style={{background: "#fff", padding: "20px"}}>
+        <Placeholder
+          description='Your message has been sent successfully.'
+          header='Success'
+        />
+        <div
+          style={{
+            padding: "20px 0",
+            textAlign: "center",
+          }}
+        >
+          <Button
+            mode='filled'
+            size='s'
+            style={{marginTop: "20px"}}
+            onClick={onClose}
+          >
+            Close
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default SuccessModal;
