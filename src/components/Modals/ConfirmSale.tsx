cecfca3e9ee26767449156131b4de896e1b9a565
lmangall <l.mangallon@gmail.com>
2024-06-16 00:23:36 +0200

    feat (ConfirmSale)

---
+++ src/components/Modals/ConfirmSale.tsx
@@ -0,0 +1,77 @@
+import React, {useState} from "react";
+import {Modal, Button, Placeholder, Checkbox} from "@telegram-apps/telegram-ui";
+
+type ConfirmSaleProps = {
+  onClose: () => void;
+  selectedChats: number;
+  word: string;
+  backendUrl: string;
+};
+
+const ConfirmSale: React.FC<ConfirmSaleProps> = ({
+  onClose,
+  selectedChats,
+  word,
+  backendUrl,
+}) => {
+  const [agreed, setAgreed] = useState(false);
+
+  const sendAgree = async () => {
+    try {
+      const response = await fetch(`${backendUrl}/add-user-to-agreed`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({agreed: true}),
+      });
+
+      if (response.status === 202) {
+        console.log("This was the last user to agree: sale successful");
+      } else {
+        console.log("Agreement recorded, but not the last user.");
+      }
+    } catch (error) {
+      console.error("Error sending agreement:", error);
+    }
+  };
+
+  return (
+    <Modal
+      header={<Modal.Header>Confirm Sale</Modal.Header>}
+      trigger={null}
+      open={true}
+    >
+      <div style={{background: "#fff", padding: "20px"}}>
+        <Placeholder
+          description={`Do you confirm to sell the ${selectedChats} selected chats for 324 ${word}?`}
+          header='Confirm Sale'
+        />
+        <div
+          style={{
+            padding: "20px 0",
+            textAlign: "center",
+          }}
+        >
+          <Checkbox checked={agreed} onChange={() => setAgreed(!agreed)} />
+          <Button
+            mode='filled'
+            size='m'
+            style={{marginTop: "20px"}}
+            onClick={sendAgree}
+            disabled={!agreed}
+          >
+            Confirm
+          </Button>
+        </div>
+        <div style={{textAlign: "center", marginTop: "20px"}}>
+          <Button mode='outline' size='s' onClick={onClose}>
+            Close
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default ConfirmSale;
