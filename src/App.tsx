ae39e77676a1cadf6d6b22fef58b2772aacc2b2f
lmangall <l.mangallon@gmail.com>
2024-06-15 16:12:06 +0200

    feat(notes)

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,10 @@ const AppContent: React.FC = () => {
   const [showOnboard, setShowOnboard] = useState<boolean>(false);
   const {user} = useUserContext();
 
+  //user doesn’t have a profile and have at least one chat  => he has been invited to sell a chat
+  //user have a profile => show his chats (+ in future show pending)
+  //user doesn’t have a profile and doesn't have a chat  => he just opened the name
+
   useEffect(() => {
     if (user.id) {
       console.log("User exists in the database, showing OnboardUserB modal");
