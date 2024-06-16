4aaf9c504f4ebe8a56a4ffc7ebc3705306b9dbe6
552020 <stefanolombardo@posteo.de>
2024-06-16 16:27:56 +0200

    refactor(Chats.tsx): clean up

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,40 +1,27 @@
-import React, {useState} from "react";
+import React from "react";
 import ChatTable from "./ChatTable";
 import Login from "./Login";
 import {useUserContext} from "../utils/utils";
 
 const Chats: React.FC = () => {
   const {user} = useUserContext();
-  const [selectedChats, setSelectedChats] = useState<{[key: string]: number}>(
-    {},
-  );
 
   const backendUrl =
     import.meta.env.VITE_BACKEND_URL ||
     "https://daniilbot-k9qlu.ondigitalocean.app";
 
+  // Needed for the other Login component
   const handleLoginSuccess = () => {
-    // Not doing anything
-    // We keep handleLoginSuccess as prop of Login only because it is used in the Home component
     console.log("Login successful");
     console.log("ChatTable rendered");
   };
 
-  const handleChatSelectionChange = (selected: {[key: string]: number}) => {
-    setSelectedChats(selected);
-  };
-
-  console.log("Chats rendered with selected chats:", selectedChats);
   return (
     <div className='p-5 max-w-xl mx-auto text-center'>
       {user.chats && user.chats.length > 0 ? (
         <div>
           <h2>Your data, your consent, your money</h2>
-          <ChatTable
-            onSelectionChange={handleChatSelectionChange}
-            selectedChats={selectedChats}
-            user={user}
-          />
+          <ChatTable user={user} />
         </div>
       ) : (
         <Login onLoginSuccess={handleLoginSuccess} backendUrl={backendUrl} />
