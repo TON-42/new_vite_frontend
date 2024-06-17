30bfb32a51fb327114e014f1dda6cb5db72ea1fa
552020 <stefanolombardo@posteo.de>
2024-06-18 01:14:22 +0200

    test(UI/Storybook): transform preview.js into .tsx and move MockUserProvider from Home.stories to preview.tsx

---
+++ src/.storybook/preview.tsx
@@ -0,0 +1,53 @@
+// .storybook/preview.tsx
+import React from "react";
+import {Preview} from "@storybook/react";
+import {UserProviderProps, UserContext} from "../components/UserContext";
+import {User} from "../types";
+
+// Mock User Context
+const mockUser: User = {
+  id: 1,
+  name: "Test User",
+  telephoneNumber: "0000000001",
+  has_profile: false,
+  chats: [
+    {
+      id: 1,
+      name: "Chat 1",
+      lead_id: 2,
+      agreed_users: [1, 2, 3],
+      status: "active",
+      words: 100,
+      users: [],
+    },
+  ],
+};
+
+const MockUserProvider: React.FC<UserProviderProps> = ({children}) => {
+  const [user, setUser] = React.useState<User>(mockUser);
+  return (
+    <UserContext.Provider value={{user, setUser}}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    Story => (
+      <MockUserProvider>
+        <Story />
+      </MockUserProvider>
+    ),
+  ],
+};
+
+export default preview;
--- src/stories/Home.stories.tsx
+++ src/stories/Home.stories.tsx
@@ -1,42 +1,10 @@
-// src/stories/Home.stories.tsx
 import {Meta, StoryObj} from "@storybook/react";
-import {UserProvider} from "../components/UserContext";
 import Home from "../components/Home";
-import {UserContextProps} from "../components/UserContext";
-
-// Mock User Context
-const mockUserContext: UserContextProps = {
-  user: {
-    id: 1,
-    name: "Test User",
-    telephoneNumber: "0000000001",
-    has_profile: false,
-    chats: [
-      {
-        id: 1,
-        name: "Chat 1",
-        lead_id: 2,
-        agreed_users: [1, 2, 3],
-        status: "active",
-        words: 100,
-        users: [],
-      },
-    ],
-  },
-  setUser: () => {},
-};
 
 const meta: Meta<typeof Home> = {
   title: "Home/Invitee",
   component: Home,
-  decorators: [
-    Story => (
-      <UserProvider value={mockUserContext}>
-        <Story />
-      </UserProvider>
-    ),
-  ],
-} satisfies Meta<typeof Home>;
+};
 
 export default meta;
 
