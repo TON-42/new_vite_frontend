8b3d5a66bad78f1da00ef919bbe186346d9de92b
552020 <stefanolombardo@posteo.de>
2024-06-17 12:29:09 +0200

    test(vitest): update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import {configDefaults} from "vitest/config";
+import "@testing-library/jest-dom"; // Directly import the setup
 
 export default defineConfig({
   plugins: [
@@ -12,4 +14,10 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: [],
+    exclude: [...configDefaults.exclude, "e2e/*"],
+  },
 });
