bec72257937b76cac789ee93140c6913770bfe8d
lmangall <l.mangallon@gmail.com>
2024-06-18 11:33:36 +0200

    fix(vercel deployment)

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,115 +1,232 @@
-import React, {useEffect, useState} from "react";
+import React, {useState, useEffect} from "react";
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
-import {CSSTransition, TransitionGroup} from "react-transition-group";
-import "./App.css";
-import logo from "./assets/logo_blink_whitebackground.gif";
-import LoadingScreen from "./Components/LoadingScreen";
-import Profile from "./tabs/Profile";
-import Referral from "./tabs/Referral";
-import Social from "./tabs/Social";
+  Button,
+  Input,
+  Checkbox,
+  Placeholder,
+  PinInput,
+} from "@telegram-apps/telegram-ui";
+import {useUserContext} from "../utils/utils";
 
-const MainScreen: React.FC = () => (
-  <div className='custom-bg text-white min-h-screen flex flex-col items-center justify-center flex-grow'>
-    <div className='Container flex flex-col items-center border-2 border-white p-4'>
-      <img src={logo} alt='Logo' className='mb-4 max-w-full h-auto' />
-      <a
-        href='https://t.me/chatpayapp_bot/chatpayapp'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='text-blue-500 underline text-4xl font-bold mb-4'
-      >
-        NEW VERSION
-      </a>
-      <p className='mb-4 text-center'>
-        You landed on an outdated version, we have a new UI and are testing our
-        alpha version.
-        <br />
-        You can also join our{" "}
-        <a
-          href='https://discord.com/invite/sNCfQcXk'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-blue-500 underline'
-        >
-          Discord
-        </a>
-        ,{" "}
-        <a
-          href='https://t.me/ChatPayApp'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-blue-500 underline'
-        >
-          Telegram Channel
-        </a>
-        ,{" "}
-        <a
-          href='https://t.me/ChatPayCommunity'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-blue-500 underline'
-        >
-          Telegram Chat Group
-        </a>
-        , and follow us on{" "}
-        <a
-          href='https://x.com/chatpay_app'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='text-blue-500 underline'
-        >
-          X
-        </a>
-        .
-      </p>
-    </div>
-  </div>
-);
+interface Chat {
+  lead_id: number;
+  agreed_users: number[];
+  name: string;
+  id: number;
+  status: string;
+  words: number;
+  users: User[];
+}
 
-const AppContent: React.FC = () => {
-  const location = useLocation();
-  const [appLoading, setAppLoading] = useState(true);
+interface User {
+  id: number;
+  chats: Chat[];
+  // Add the rest of the properties as needed
+}
+interface LoginProps {
+  onLoginSuccess: () => void;
+  backendUrl: string;
+}
+
+const Login: React.FC<LoginProps> = ({onLoginSuccess, backendUrl}) => {
+  const [phone, setPhone] = useState("");
+  const [, setPin] = useState<number[]>([]);
+  const [isPhoneSubmitted, setIsPhoneSubmitted] = useState(false);
+  const [agreed, setAgreed] = useState(false);
+  const [responseMessage, setResponseMessage] = useState<string | null>(null);
+  const [pinString, setPinString] = useState("");
+
+  const {user, setUser} = useUserContext(); // Use the context
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPhone(event.target.value);
+  };
+
+  const handlePinChange = (value: number[]) => {
+    setPin(value);
+    setPinString(value.join("")); // Update the pinString whenever pin changes
+  };
+
+  const sendPhoneNumber = async () => {
+    try {
+      console.log("Sending phone number:", phone);
+      const response = await fetch(`${backendUrl}/send-code`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({phone_number: phone}),
+      });
+
+      if (!response.ok) {
+        const errorMessage = await response.text();
+        console.error("Error message:", errorMessage);
+        throw new Error(errorMessage);
+      }
+
+      setIsPhoneSubmitted(true);
+      setResponseMessage("Verification code sent. Please check your phone.");
+      console.log("Verification code sent successfully");
+    } catch (error) {
+      console.error("Error sending phone number:", error);
+      setResponseMessage("Error sending phone number");
+    }
+  };
 
   useEffect(() => {
-    // Simulate a loading process for 4 seconds
-    setTimeout(() => {
-      setAppLoading(false);
-    }, 4000);
-  }, []);
+    const verifyCode = async () => {
+      try {
+        console.log("Verifying code:", pinString);
+        const response = await fetch(`${backendUrl}/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            phone_number: phone,
+            code: pinString,
+          }),
+        });
 
-  if (appLoading) {
-    return <LoadingScreen />;
-  }
+        if (!response.ok) {
+          const errorMessage = await response.text();
+          console.error("Error message:", errorMessage);
+          throw new Error(errorMessage);
+        }
+
+        const responseData = await response.json();
+        const chats = responseData;
+        console.log(chats);
+        setResponseMessage("Success");
+
+        console.log("User (context) before setting:", user);
+
+        const formattedChats = transformData(chats);
+
+        setUser(prevUser => {
+          const newUser = {
+            ...prevUser,
+            telephoneNumber: phone,
+            chats: formattedChats,
+            has_profile: true,
+          };
+
+          console.log("User (context) after setting:", newUser);
+
+          return newUser;
+        });
+
+        onLoginSuccess();
+      } catch (error) {
+        console.error("Error verifying code:", error);
+        setResponseMessage("Error verifying code");
+      }
+    };
+
+    if (pinString.length === 5) {
+      verifyCode();
+    }
+  }, [pinString, phone, backendUrl, setUser, onLoginSuccess, user]);
+
+  const transformData = (data: {[key: string]: number}): Chat[] => {
+    const chats: Chat[] = [];
+
+    // Iterate over the data entries
+    for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        // Extract the userId and userName from the key
+        const keyParts = key.match(/\((\d+), '(.+?)'\)/);
+        if (keyParts && keyParts.length === 3) {
+          const userId = parseInt(keyParts[1], 10); // Parse id as number
+          const userName = keyParts[2];
+          const words = data[key];
+
+          // Create a Chat object and add it to the array
+          chats.push({
+            id: userId,
+            name: userName,
+            words,
+            lead_id: 0, // Default or modify as needed
+            agreed_users: [], // Default or modify as needed
+            status: "", // Default or modify as needed
+            users: [], // Default or modify as needed
+          });
+        }
+      }
+    }
+
+    return chats;
+  };
 
   return (
-    <div className='App'>
-      <TransitionGroup>
-        <CSSTransition key={location.key} classNames='slide' timeout={300}>
-          <div>
-            <Routes location={location}>
-              <Route path='/' element={<MainScreen />} />
-              <Route path='/profile' element={<Profile />} />
-              <Route path='/referral' element={<Referral />} />
-              <Route path='/social' element={<Social />} />
-            </Routes>
-          </div>
-        </CSSTransition>
-      </TransitionGroup>
+    <div
+      style={{
+        padding: "20px",
+        maxWidth: "400px",
+        margin: "auto",
+        textAlign: "center",
+      }}
+    >
+      {!isPhoneSubmitted ? (
+        <>
+          <Placeholder description='Please log in to continue' header='Login' />
+          <Input
+            header='Phone Number'
+            placeholder='Enter your phone number'
+            value={phone}
+            onChange={handleInputChange}
+            type='tel'
+          />
+          <Placeholder>
+            <div style={{display: "flex", alignItems: "center"}}>
+              <Checkbox
+                checked={agreed}
+                onChange={() => setAgreed(!agreed)}
+                style={{marginRight: "8px"}}
+              />
+              <span style={{whiteSpace: "nowrap"}}>
+                I agree to the{" "}
+                <a
+                  href='https://static1.squarespace.com/static/665b166b65c61d1f819dec7e/t/665c43d3be949513ba28488c/1717322707955/USER+AGREEMENT.pdf'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  terms and conditions
+                </a>
+                .
+              </span>
+            </div>
+          </Placeholder>
+          <Button onClick={sendPhoneNumber} size='m' disabled={!agreed}>
+            Submit
+          </Button>
+          {/* <Button
+            onClick={handleDebugLogin}
+            size='m'
+            style={{marginTop: "10px"}}
+          >
+            Debug Login
+          </Button> */}
+        </>
+      ) : (
+        <>
+          <Placeholder
+            description='Enter the code sent to your phone'
+            header='Verification Code'
+          />
+          <PinInput pinCount={5} onChange={handlePinChange} />
+          {/* <Button
+            onClick={handleDebugLogin}
+            size='m'
+            style={{marginTop: "10px"}}
+          >
+            Debug Login
+          </Button> */}
+        </>
+      )}
+      {responseMessage && <p className='mt-4 text-white'>{responseMessage}</p>}
     </div>
   );
 };
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-};
-
-export default App;
+export default Login;
