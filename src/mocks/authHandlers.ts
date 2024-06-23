6ab7fe55986f0bc4c3bb6b6c3375bda8063f910a
552020 <stefanolombardo@posteo.de>
2024-06-23 11:01:08 +0200

    chore: remove commented out code

--- src/mocks/authHandlers.ts
+++ src/mocks/authHandlers.ts
@@ -3,36 +3,12 @@ import {http, HttpResponse} from "msw";
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-// interface SendCodeRequestBody {
-//   phone: string;
-// }
-
 interface LoginRequestBody {
   phone: string;
   code: string;
 }
 
 export const authHandlers = [
-  //   http.post(`${backendUrl}/send-code`, async ({request}) => {
-  //     const json = await request.json();
-
-  //     if (!json || typeof json !== "object") {
-  //       return new HttpResponse("Invalid request body", {
-  //         status: 400,
-  //         headers: {"Content-Type": "application/json"},
-  //       });
-  //     }
-
-  //     const body = json as SendCodeRequestBody;
-  //     // Simulate sending code
-  //     console.log(`Code sent to phone number: ${body.phone}`);
-
-  //     return new HttpResponse("Code sent", {
-  //       status: 200,
-  //       headers: {"Content-Type": "application/json"},
-  //     });
-  //   }),
-
   http.post(`${backendUrl}/login`, async ({request}) => {
     const json = await request.json();
 
@@ -44,13 +20,11 @@ export const authHandlers = [
     }
 
     const body = json as LoginRequestBody;
-    // Simulate login process
     console.log(
       `Login attempt with phone: ${body.phone} and code: ${body.code}`,
     );
 
     if (body.code === "1234") {
-      // Example code validation
       return new HttpResponse("Login successful", {
         status: 200,
         headers: {"Content-Type": "application/json"},
