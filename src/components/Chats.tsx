265f9a2c3470b52f0a295a0de78dca3e0763114f
lmangall <l.mangallon@gmail.com>
2024-06-24 16:50:00 +0200

    fix(flow)

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -8,7 +8,7 @@ import {List, Chip} from "@telegram-apps/telegram-ui";
 const Chats: React.FC<{backendUrl: string}> = ({backendUrl}) => {
   const {user, isLoggedIn} = useUserContext(); // Access the user context
   const [showChatTable, setShowChatTable] = useState<boolean>(false);
-  const [showChatTableUserB, setShowChatTableUserB] = useState<boolean>(false);
+  const [showChatTableUserB, setShowChatTableUserB] = useState<boolean>(true);
   const [showLogin, setShowLogin] = useState<boolean>(false);
 
   // Update state based on user profile and chats
@@ -26,7 +26,7 @@ const Chats: React.FC<{backendUrl: string}> = ({backendUrl}) => {
         console.log("User has a profile, showing ChatTable");
       } else {
         setShowChatTable(false);
-        setShowChatTableUserB(false);
+        setShowChatTableUserB(true); // Show ChatTableUserB by default for non-logged-in users
       }
     }
   }, [user]);
@@ -35,6 +35,7 @@ const Chats: React.FC<{backendUrl: string}> = ({backendUrl}) => {
     console.log("Login successful");
     setShowLogin(false);
     setShowChatTable(true);
+    setShowChatTableUserB(false);
   };
 
   const handleMyChatsClick = () => {
@@ -54,40 +55,28 @@ const Chats: React.FC<{backendUrl: string}> = ({backendUrl}) => {
 
   return (
     <div className='p-5 max-w-xl mx-auto text-center'>
-      {isLoggedIn || showLogin ? (
-        user.chats && user.chats.length > 0 ? (
-          <div className='items-center '>
-            <List className='p-5 bg-gray-100 rounded-lg shadow mb-4 '>
-              <div className='flex gap-4'>
-                <Chip
-                  mode={showChatTable ? "elevated" : "mono"}
-                  after={<span className='chip-icon'>👉</span>}
-                  onClick={handleMyChatsClick}
-                >
-                  My chats
-                </Chip>
-                <Chip
-                  mode={showChatTableUserB ? "elevated" : "mono"}
-                  after={<span className='chip-icon'>📧</span>}
-                  onClick={handleMyInvitationsClick}
-                >
-                  My invitations
-                </Chip>
-              </div>
-            </List>
-            <div className='w-full'>
-              {showChatTable && (
-                <ChatTable user={user} backendUrl={backendUrl} />
-              )}
-              {showChatTableUserB && <ChatTableUserB backendUrl={backendUrl} />}
-            </div>
-          </div>
-        ) : (
-          <p>No chats available</p>
-        )
-      ) : (
-        <Login onLoginSuccess={handleLoginSuccess} backendUrl={backendUrl} />
-      )}
+      <List className='p-5 bg-gray-100 rounded-lg shadow mb-4 '>
+        <div className='flex gap-4'>
+          <Chip
+            mode={showChatTable ? "elevated" : "mono"}
+            after={<span className='chip-icon'>👉</span>}
+            onClick={handleMyChatsClick}
+          >
+            My chats
+          </Chip>
+          <Chip
+            mode={showChatTableUserB ? "elevated" : "mono"}
+            after={<span className='chip-icon'>📧</span>}
+            onClick={handleMyInvitationsClick}
+          >
+            My invitations
+          </Chip>
+        </div>
+      </List>
+      <div className='w-full'>
+        {showChatTable && <ChatTable user={user} backendUrl={backendUrl} />}
+        {showChatTableUserB && <ChatTableUserB backendUrl={backendUrl} />}
+      </div>
       {showLogin && (
         <Login onLoginSuccess={handleLoginSuccess} backendUrl={backendUrl} />
       )}
