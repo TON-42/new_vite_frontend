e778350658dcd00c2b9a1dff091d644f5ddfccde
lmangall <l.mangallon@gmail.com>
2024-07-01 16:56:08 +0200

    fix:(login): remove login from home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,6 @@
-// Home.tsx
 import React, {useState, useEffect} from "react";
-import {Blockquote, Timeline, Text, Button} from "@telegram-apps/telegram-ui";
+import {Blockquote, Timeline, Text} from "@telegram-apps/telegram-ui";
 import OnboadUserB from "./Modals/OnboardUserB";
-import Login from "./Login";
 import {useUserContext} from "../utils/utils";
 
 interface HomeProps {
@@ -10,10 +8,9 @@ interface HomeProps {
   backendUrl: string;
 }
 
-const Home: React.FC<HomeProps> = ({setCurrentTab, backendUrl}) => {
+const Home: React.FC<HomeProps> = ({setCurrentTab}) => {
   const {user} = useUserContext();
   const [showSaleInfo] = useState(false);
-  const [showLogin, setShowLogin] = useState(false);
   const [showOnboardUserB, setShowOnboardUserB] = useState(false);
 
   useEffect(() => {
@@ -30,58 +27,36 @@ const Home: React.FC<HomeProps> = ({setCurrentTab, backendUrl}) => {
     setCurrentTab("chats");
   };
 
-  const handleLoginSuccess = () => {
-    setShowLogin(false);
-    setCurrentTab("chats");
-  };
-
   return (
-    // <div className='flex justify-center items-center'>
-    // <div className='border-black border-2 p-2'>
     <div className='flex flex-col min-h-screen p-5'>
-      {showLogin ? (
-        <Login onLoginSuccess={handleLoginSuccess} backendUrl={backendUrl} />
-      ) : (
-        <>
-          <div className='flex-1'>
-            <h1 className='text-4xl font-bold mb-8'>
-              {user.name ? `Hello, ${user.name}!` : "Heiya!"} 👋
-            </h1>
-            <Text className='font-medium mb-4 p-4'>
-              ChatPay provides users an easy way to earn money from their
-              existing Telegram chats by bundling them into AI training
-              datasets.
-            </Text>
+      <div className='flex-1'>
+        <h1 className='text-4xl font-bold mb-8'>
+          {user.name ? `Hello, ${user.name}!` : "Heiya!"} 👋
+        </h1>
+        <Text className='font-medium mb-4 p-4'>
+          ChatPay provides users an easy way to earn money from their existing
+          Telegram chats by bundling them into AI training datasets.
+        </Text>
 
-            <div className='mb-8 p-4'>
-              <Blockquote type='text'>
-                🙅 NO personal data is collected.
-              </Blockquote>
-            </div>
-            <div className='flex justify-center items-center mb-8'>
-              <Button onClick={() => setShowLogin(true)} mode='filled' size='m'>
-                Login
-              </Button>
-            </div>
-            <Timeline active={4} style={{textAlign: "left"}}>
-              <Timeline.Item header='Check chats value'>
-                Your chats are worth money
-              </Timeline.Item>
-              <Timeline.Item header='Pick chats you want to sell'>
-                All data is anonymized
-              </Timeline.Item>
-              <Timeline.Item header='Wait for friends to accept'>
-                Everyone has to accept
-              </Timeline.Item>
-              <Timeline.Item header='Get the money'>
-                Profits are shared equally
-              </Timeline.Item>
-            </Timeline>
-            {/* {showSaleInfo && <SaleInfo />} */}
-            {showSaleInfo && <p> SaleInfo</p>}
-          </div>
-        </>
-      )}
+        <div className='mb-8 p-4'>
+          <Blockquote type='text'>🙅 NO personal data is collected.</Blockquote>
+        </div>
+        <Timeline active={4} style={{textAlign: "left"}}>
+          <Timeline.Item header='Check chats value'>
+            Your chats are worth money
+          </Timeline.Item>
+          <Timeline.Item header='Pick chats you want to sell'>
+            All data is anonymized
+          </Timeline.Item>
+          <Timeline.Item header='Wait for friends to accept'>
+            Everyone has to accept
+          </Timeline.Item>
+          <Timeline.Item header='Get the money'>
+            Profits are shared equally
+          </Timeline.Item>
+        </Timeline>
+        {showSaleInfo && <p> SaleInfo</p>}
+      </div>
       {showOnboardUserB && <OnboadUserB onClose={handleOnboardClose} />}
     </div>
   );
