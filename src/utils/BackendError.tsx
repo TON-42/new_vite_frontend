3c3c709c925b50a75a46bc7b06bc9d240e9f49ad
lmangall <l.mangallon@gmail.com>
2024-07-03 21:16:07 +0200

    refactor(BackendError):  accept an error code and execute a callback function to switch tab

--- src/utils/BackendError.tsx
+++ src/utils/BackendError.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 interface BackendErrorProps {
   message: string;
+  errorCode: number;
   onClose: () => void;
+  onRedirect: () => void;
 }
 
-const BackendError: React.FC<BackendErrorProps> = ({message, onClose}) => {
+const BackendError: React.FC<BackendErrorProps> = ({
+  message,
+  errorCode,
+  onClose,
+  onRedirect,
+}) => {
+  useEffect(() => {
+    if (errorCode === 555) {
+      onRedirect();
+    }
+  }, [errorCode, onRedirect]);
+
   return (
     <div className='fixed top-0 left-0 right-0 bg-red-500 text-white p-4 z-50'>
       <div className='container mx-auto flex justify-between items-center'>
