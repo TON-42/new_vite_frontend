de3f7c694596809297e6454666ea7c8fe90f9a11
lmangall <l.mangallon@gmail.com>
2024-07-07 23:05:22 +0200

    fix double home, try another emoji source

--- src/App.tsx
+++ src/App.tsx
@@ -123,7 +123,6 @@ const AppContent: React.FC = () => {
       <div className='flex flex-col items-center p-5 max-w-full mx-auto text-center mt-24'>
         <div className='flex-1 w-full max-w-4xl'>
           {currentTab === "home" && <Home />}
-          {currentTab === "home" && <Home />}
           {currentTab === "chats" && <Chats backendUrl={backendUrl} />}
           {currentTab === "social" && <Social />}
           {currentTab === "quest" && <Quest />}
