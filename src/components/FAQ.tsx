cf5efa7d904bf4ba6ae51ee8481dc3afb6f3a33a
552020 <stefanolombardo@posteo.de>
2024-07-12 15:56:16 +0200

    chore: install marked

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,16 +1,55 @@
 import React, {useState, useEffect} from "react";
+import marked from "marked";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
+interface FaqItem {
+  question: string;
+  answer: string;
+}
+
 export const FAQ: React.FC = () => {
   const [content, setContent] = useState<string>("");
+  //   const [faqItems, setFaqItems] = useState([]);
+  const [faqItems, setFaqItems] = useState<FaqItem[]>([]);
 
   useEffect(() => {
     fetch("/docs/FAQ.md")
       .then(response => response.text())
       .then(text => setContent(text));
+
+    const fetchMarkdown = async () => {
+      try {
+        const response = await fetch("/docs/FAQ.md");
+        const markdownContent = await response.text();
+
+        const faqArray = parseMarkdown(markdownContent);
+        setFaqItems(faqArray);
+      } catch (error) {
+        console.log("Error fetching or parsing FAQ.md");
+      }
+    };
+    fetchMarkdown();
   }, []);
 
+  const parseMarkdown = markdownContent => {
+    const tokens = marked.lexer(markdownContent);
+    const faqArray = [];
+
+    let currentQuestion = null;
+    let currentAnswer = null;
+
+    tokens.forEach(token => {
+      if (token.type === "heading" && token.depth === 2) {
+        currentQuestion = token.text.trim();
+      } else if (token.type === "paragraph" && currentQuestion) {
+        currentAnswer = token.text.trim();
+        faqArray.push({question: currentQuestion, answer: currentAnswer});
+        currentQuestion = null;
+        currentAnswer = null;
+      }
+    });
+  };
   return (
     <div className='mx-auto p-4 max-w-4xl prose prose-sm text-left'>
       <ReactMarkdown className='prose prose-lg' remarkPlugins={[remarkGfm]}>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,47 +3,52 @@ import {Text, Card} from "@telegram-apps/telegram-ui";
 import {TonConnectUIProvider, TonConnectButton} from "@tonconnect/ui-react";
 import logo from "../assets/logo_whitebackground.png";
 import {useUserContext} from "../utils/utils";
+import {FAQ} from "./FAQ";
 
 const Home: React.FC = () => {
   const {user} = useUserContext();
   const balance = user.words ? user.words : 0;
 
   return (
-    <div className='flex flex-col p-4'>
-      <header className='flex justify-center items-center mb-4'>
-        <img src={logo} alt='Logo' className='w-42' />
-      </header>
-      <h1 className='text-4xl font-bold mb-4 text-center'>
-        {user.name ? `Hello, ${user.name}!` : "Heiya!"} 👋
-      </h1>
-      <Text className='font-medium mb-2 text-center'>
-        ChatPay empowers you to sell your Telegram chat data
-      </Text>
-      <div className='text-2xl mb-4 text-center'>💬 = 💰</div>
-      <div className='flex justify-between items-center mb-4'>
-        <div className='text-left'>
-          Your points Balance is the amount you earn by selling your chat data.
+    <div>
+      <div className='flex flex-col p-4'>
+        <header className='flex justify-center items-center mb-4'>
+          <img src={logo} alt='Logo' className='w-42' />
+        </header>
+        <h1 className='text-4xl font-bold mb-4 text-center'>
+          {user.name ? `Hello, ${user.name}!` : "Heiya!"} 👋
+        </h1>
+        <Text className='font-medium mb-2 text-center'>
+          ChatPay empowers you to sell your Telegram chat data
+        </Text>
+        <div className='text-2xl mb-4 text-center'>💬 = 💰</div>
+        <div className='flex justify-between items-center mb-4'>
+          <div className='text-left'>
+            Your points Balance is the amount you earn by selling your chat
+            data.
+          </div>
+          <div className='flex-1 ml-4'>
+            <Card type='plain'>
+              <React.Fragment key='.0'>
+                <Card.Cell readOnly subtitle={`${balance} $WORDS`}>
+                  {/* Balance */}
+                </Card.Cell>
+              </React.Fragment>
+            </Card>
+          </div>
         </div>
-        <div className='flex-1 ml-4'>
-          <Card type='plain'>
-            <React.Fragment key='.0'>
-              <Card.Cell readOnly subtitle={`${balance} $WORDS`}>
-                {/* Balance */}
-              </Card.Cell>
-            </React.Fragment>
-          </Card>
+        <div className='mt-auto flex justify-center items-center'>
+          <TonConnectUIProvider
+            manifestUrl='https://yourappurl.com/tonconnect-manifest.json'
+            actionsConfiguration={{
+              twaReturnUrl: "https://t.me/chatpayapp_bot/chatpayapp",
+            }}
+          >
+            <TonConnectButton className='my-button-class' />
+          </TonConnectUIProvider>
         </div>
       </div>
-      <div className='mt-auto flex justify-center items-center'>
-        <TonConnectUIProvider
-          manifestUrl='https://yourappurl.com/tonconnect-manifest.json'
-          actionsConfiguration={{
-            twaReturnUrl: "https://t.me/chatpayapp_bot/chatpayapp",
-          }}
-        >
-          <TonConnectButton className='my-button-class' />
-        </TonConnectUIProvider>
-      </div>
+      <FAQ />
     </div>
   );
 };
