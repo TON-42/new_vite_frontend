b83a241b060fb002a9e9243dbd7bb67865372ed9
Danielg1406 <danielgomeza1406@gmail.com>
2024-07-23 16:09:57 +0200

    fix Home: activate claim card to test it on production

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -84,7 +84,7 @@ const Home: React.FC = () => {
           buttonText='Claim'
           buttonMode='filled'
           buttonOnClick={handleClaimClick}
-          isActive={false}
+          // isActive={false}
         />
         <HomeCard
           header='Daily Quest'
